de40d3cb0a08dfb8781022681bc9f5f8e7249356
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2021-05-03 02:08:38 -0400

    Updated social media icons

---
+++ src/colors.js
@@ -0,0 +1,11 @@
+const primaryDark = '#00675b',
+    primaryDefault = '#009688',
+    primaryLight = '#52c7b8',
+    secondaryDefault = '#ffc107';
+
+export {
+    primaryDark,
+    primaryDefault,
+    primaryLight,
+    secondaryDefault,
+};
