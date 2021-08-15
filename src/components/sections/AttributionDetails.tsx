9fae66f9e2edf8a9e63d248e3d4d4c8e424918e1
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2021-08-15 03:16:29 -0400

    Added photographer attribution

--- src/components/sections/AttributionDetails.tsx
+++ src/components/sections/AttributionDetails.tsx
@@ -20,15 +20,47 @@ interface AttributionDetailsInterface {
 }
 
 const AttributionDetails = () => {
-    const devLink = <a href="https://www.linkedin.com/in/christopher-flinchbaugh/" target="_blank" rel="noopener noreferrer">Chris Flinchbaugh</a>;
-    const freePikLink = <a href="https://www.freepik.com" title="Freepik" target="_blank" rel="noopener noreferrer">Freepik</a>;
-    const flitIconLink = <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noopener noreferrer">www.flaticon.com</a>;
+    const devLink = (
+            <a href="https://www.linkedin.com/in/christopher-flinchbaugh/" 
+                title="Chris Flinchbaugh"
+                target="_blank" 
+                rel="noopener noreferrer">
+                    Chris Flinchbaugh
+            </a>
+        ),
+        photographerLink = (
+            <a href="https://www.kristamaemyers.com/" 
+                title="Krista Mae Myers"
+                target="_blank" 
+                rel="noopener noreferrer">
+                    Krista Mae Myers
+            </a>
+        ),
+        freePikLink = (
+            <a href="https://www.freepik.com" 
+                title="Freepik" 
+                target="_blank" 
+                rel="noopener noreferrer">
+                    Freepik
+            </a>
+        ),
+        flitIconLink = (
+            <a href="https://www.flaticon.com/" 
+                title="Flaticon" 
+                target="_blank" 
+                rel="noopener noreferrer">
+                    www.flaticon.com
+            </a>
+        );
 
     return (
         <StyleWrapper>
             <ul>
                 <li>
                     Designed & Developed by {devLink}
+                </li>
+                <li>
+                    Photos by {photographerLink}
                 </li>
                 <li>
                     Icons made by {freePikLink} from {flitIconLink}
